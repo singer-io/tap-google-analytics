410b0e2de3255f9b4cfbf53f3517376319c85af6
Dan Mosora <dan@stitchdata.com>
2020-01-14 22:25:47 +0000

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-google-analytics",
-    version="0.0.1",
+    version="0.1.0",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
