225a022038284850023f6e9e0a66e1ac3f030871
Dan Mosora <30501696+dmosorast@users.noreply.github.com>
2020-02-26 10:19:08 -0500

    bump version; add changelog (#12)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-google-analytics",
-    version="0.1.1",
+    version="0.2.0",
     description="Singer.io tap for extracting data",
     author="Stitch",
     url="http://singer.io",
