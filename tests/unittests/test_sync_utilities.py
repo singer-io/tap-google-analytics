cd4af2ebd59f08073b9e2db326af34058601d2d4
Dan Mosora <30501696+dmosorast@users.noreply.github.com>
2020-10-23 17:32:41 +0000

    Make new test fail, and add interrupted test

--- tests/unittests/test_sync_utilities.py
+++ tests/unittests/test_sync_utilities.py
@@ -5,9 +5,16 @@ from singer import utils
 import tap_google_analytics.sync
 from tap_google_analytics.sync import sync_report, generate_sdc_record_hash
 
+# Test State Tracking Globals
 reports = None
+error_after = None
+reports_synced = 0
 
 def get_mock_report(name, profile_id, report_date, metrics, dimensions):
+    global reports_synced
+    if error_after and reports_synced == error_after:
+        raise Exception("Report failed!")
+    reports_synced += 1
     return reports[report_date]
 
 class TestIsDataGoldenBookmarking(unittest.TestCase):
@@ -23,8 +30,13 @@ class TestIsDataGoldenBookmarking(unittest.TestCase):
         self.client.get_report = MagicMock(side_effect=get_mock_report)
 
     def tearDown(self):
+        # Reset globals
         global reports
+        global error_after
+        global reports_synced
         reports = None
+        error_after = None
+        reports_synced = 0
 
     @patch("tap_google_analytics.sync.report_to_records")
     @patch("singer.write_record")
@@ -60,11 +72,19 @@ class TestIsDataGoldenBookmarking(unittest.TestCase):
     @patch("singer.write_record")
     @patch("singer.write_state")
     def test_historical_sync_writes_bookmarks_and_stops_at_first_non_golden(self, *args):
+        # We have observed that if there's no historical data, Google will
+        # return a null isDataGolden field. This caused the tap to never
+        # save properties.
+
+        # Expectation: Full end to end historical sync should function as above.
         global reports
         reports = {
-            utils.strptime_to_utc("2019-10-30"): [{"reports": [{"data": {"isDataGolden": True}}]}],
-            utils.strptime_to_utc("2019-10-31"): [{"reports": [{"data": {"isDataGolden": True}}]}],
-            **reports
+            utils.strptime_to_utc("2019-10-30"): [{"reports": [{"data": {}}]}],
+            utils.strptime_to_utc("2019-10-31"): [{"reports": [{"data": {}}]}],
+            utils.strptime_to_utc("2019-11-01"): [{"reports": [{"data": {"isDataGolden": True}}]}],
+            utils.strptime_to_utc("2019-11-02"): [{"reports": [{"data": {"isDataGolden": True}}]}],
+            utils.strptime_to_utc("2019-11-03"): [{"reports": [{"data": {}}]}],
+            utils.strptime_to_utc("2019-11-04"): [{"reports": [{"data": {"isDataGolden": True}}]}],
         }
         state = {}
         sync_report(self.client,
@@ -76,6 +96,37 @@ class TestIsDataGoldenBookmarking(unittest.TestCase):
                     historically_syncing=True)
         self.assertEqual({'bookmarks': {'123': {'12345': {'last_report_date': '2019-11-03'}}}}, state)
         self.assertEqual(self.client.get_report.call_count, 6)
+
+    @patch("tap_google_analytics.sync.report_to_records")
+    @patch("singer.write_record")
+    @patch("singer.write_state")
+    def test_historical_sync_interrupted_writes_bookmarks(self, *args):
+        # We have observed that if there's no historical data, Google will
+        # return a null isDataGolden field. This caused the tap to never
+        # save properties. If a tap is interrupted during this time of null
+        # golden-ness, it should resume.
+
+        # Expectation: Interrupted historical sync should resume where it left off.
+        global reports
+        global error_after
+        error_after = 3
+        reports = {
+            utils.strptime_to_utc("2019-10-30"): [{"reports": [{"data": {}}]}],
+            utils.strptime_to_utc("2019-10-31"): [{"reports": [{"data": {}}]}],
+            utils.strptime_to_utc("2019-11-01"): [{"reports": [{"data": {}}]}],
+            utils.strptime_to_utc("2019-11-02"): [{"reports": [{"data": {}}]}],
+        }
+        state = {}
+        with self.assertRaises(Exception):
+            sync_report(self.client,
+                        {},
+                        {"id": "123", "name":"test_report", "profile_id": "12345", "metrics":[], "dimensions":[]},
+                        utils.strptime_to_utc("2019-10-30"),
+                        utils.strptime_to_utc("2019-11-02"),
+                        state,
+                        historically_syncing=True)
+        self.assertEqual({'bookmarks': {'123': {'12345': {'last_report_date': '2019-11-01'}}}}, state)
+        self.assertEqual(self.client.get_report.call_count, 4)
 
 
 class TestRecordHashing(unittest.TestCase):
