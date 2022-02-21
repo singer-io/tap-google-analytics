995df05edcb4db048a1d2f1287a41927656c2d72
Umang Agrawal <umang.agrawal@crestdatasys.com>
2022-02-21 16:25:12 +0530

    Resolved review comments

--- tests/test_google_analytics_random_dimension_metric.py
+++ tests/test_google_analytics_random_dimension_metric.py
@@ -2,30 +2,34 @@ import tap_tester.connections as connections
 import tap_tester.runner as runner
 import tap_tester.menagerie as menagerie
 from base import GoogleAnalyticsBaseTest
+import random
 
 
-class GoogleAnalyticsAllFieldsTest(GoogleAnalyticsBaseTest):
-    """Ensure running the tap with all streams and fields selected results in the replication of all fields."""
-    
-    # Skipped below streams due to following issues/limitations
-    # Ecommerce Overview - TODO https://stitchdata.atlassian.net/browse/SRCE-5084
-    # Test Report 1, Audience Technology - As per this doc (https://www.stitchdata.com/docs/integrations/saas/google-analytics#track-data)
-    # we can only select max 10 metrics and 7 dimensions. So skipped these streams.
-    # Behavior Overview - Some combination of metric and dimensions are not allowed. So skipped from all fields test.
-    SKIP_STREAMS = {'Ecommerce Overview','Test Report 1', 'Audience Technology', 'Behavior Overview',}
+class GoogleAnalyticsRandomFieldselectionTest(GoogleAnalyticsBaseTest):
+    """Ensure running the tap with Audience Technology streams and fields selected results in the replication of more than automatic fields."""
 
     def name(self):
-        return "tap_tester_google_analytics_all_fields_test"
+        return "tap_tester_google_analytics_random_dimension_metric_test"
+
+    def get_non_selected_fields(self):
+        dimension = {"ga:browser","ga:operatingSystem","ga:flashVersion","ga:javaEnabled","ga:screenColors",
+                     "ga:screenResolution","ga:hostname","ga:date","ga:year","ga:month","ga:hour"}
+        selected_fields = set(random.sample(dimension, 9))  # randomly select 9 fields from dimension set
+        non_selected_fields = dimension - selected_fields
+        return non_selected_fields
 
     def test_run(self):
         """
-        • Verify no unexpected streams were replicated
+        • Verify no unexpected streams were replicated.
         • Verify that more than just the automatic fields are replicated for each stream. 
-        • verify selected fields for each stream are replicated
+        • verify the selected fields for each Audience Technology streams are replicated.
         """
 
-        # Streams to verify all fields tests
-        expected_streams = self.expected_sync_streams() - self.SKIP_STREAMS
+        expected_streams = {"Audience Technology"}
+
+        non_selected_properties_by_stream = {
+            "Audience Technology":self.get_non_selected_fields()
+            }
 
         expected_automatic_fields = self.expected_automatic_fields()
         conn_id = connections.ensure_connection(self)
@@ -36,7 +40,7 @@ class GoogleAnalyticsAllFieldsTest(GoogleAnalyticsBaseTest):
         test_catalogs_all_fields = [catalog for catalog in found_catalogs
                                           if catalog.get('stream_name') in expected_streams]
         self.perform_and_verify_table_and_field_selection(
-            conn_id, test_catalogs_all_fields, select_default_fields=True
+            conn_id, test_catalogs_all_fields, select_default_fields=False, non_selected_props=non_selected_properties_by_stream
         )
 
         # grab metadata after performing table-and-field selection to set expectations
@@ -49,7 +53,7 @@ class GoogleAnalyticsAllFieldsTest(GoogleAnalyticsBaseTest):
                                           for md_entry in catalog_entry['metadata']
                                           if md_entry['breadcrumb'] != []]
             stream_to_all_catalog_fields[stream_name] = set(
-                fields_from_field_level_md)
+                fields_from_field_level_md) - non_selected_properties_by_stream.get(catalog['stream_name'])
 
         self.run_and_verify_sync(conn_id)
 
@@ -58,7 +62,6 @@ class GoogleAnalyticsAllFieldsTest(GoogleAnalyticsBaseTest):
         # Verify no unexpected streams were replicated
         synced_stream_names = set(synced_records.keys())
         self.assertSetEqual(expected_streams, synced_stream_names)
-
 
         for stream in expected_streams:
             with self.subTest(stream=stream):
@@ -79,5 +82,5 @@ class GoogleAnalyticsAllFieldsTest(GoogleAnalyticsBaseTest):
                     if message['action'] == 'upsert':
                         actual_all_keys.update(message['data'].keys())
 
-                # verify selected fields for each stream are replicated
+                # verify all the selected fields for each Audience Technology are replicated
                 self.assertSetEqual(expected_all_keys, actual_all_keys)
