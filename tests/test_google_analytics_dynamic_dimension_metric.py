d13257c26ac6163393c62b36d021ba309d367f20
Umang Agrawal <umang.agrawal@crestdatasys.com>
2022-02-25 11:32:14 +0530

    Addressing the review comments

--- tests/test_google_analytics_dynamic_dimension_metric.py
+++ tests/test_google_analytics_dynamic_dimension_metric.py
@@ -4,33 +4,104 @@ import tap_tester.menagerie as menagerie
 from base import GoogleAnalyticsBaseTest
 import random
 
+NO_OF_DIMENSIION_ALLOWED = 9
+NO_OF_METRIC_ALLOWED = 10
 
 class GoogleAnalyticsDynamicFieldselectionTest(GoogleAnalyticsBaseTest):
-    """Ensure running the tap with Audience Technology streams and fields selected results in the replication of more than automatic fields."""
+    """Ensure running the tap with streams and fields selected results in the replication of more than automatic fields."""
 
+    stream_dimension = {
+        # Provide the dimension for streams for the random selection
+        "Audience Technology": {"ga:browser", "ga:operatingSystem", "ga:flashVersion", "ga:javaEnabled", "ga:screenColors",
+                     "ga:screenResolution", "ga:hostname", "ga:date", "ga:year", "ga:month", "ga:hour"}
+        }
+
+    conflicting_dimension = {
+        # Provide the conflicting dimension and metric 
+        # Eg: "ga:searchKeyword": {"ga:eventCategory"}
+    }
+
+    stream_metric = {
+        # Provide the metrics for streams  for the random selection
+        "Audience Technology": {"ga:users", "ga:newUsers", "ga:sessions", "ga:bounceRate", "ga:avgSessionDuration", "ga:pageviewsPerSession"}
+    }
+
+    # Ecommerce Overview - TODO https://stitchdata.atlassian.net/browse/SRCE-5084
+    # Test Report 1 - Currently, skipping as it has 200+ dimension and exclusion logic
+    SKIP_STREAMS = {'Ecommerce Overview', 'Test Report 1'}
+
+    
     @staticmethod
     def name():
         return "tt_google_analytics_dynamic_metric_dimension"
 
-    def get_non_selected_fields(self):
-        dimension = {"ga:browser","ga:operatingSystem","ga:flashVersion","ga:javaEnabled","ga:screenColors",
-                     "ga:screenResolution","ga:hostname","ga:date","ga:year","ga:month","ga:hour"}
-        selected_fields = set(random.sample(dimension, 9))  # randomly select 9 fields from dimension set
-        non_selected_fields = dimension - selected_fields
+    def get_non_selected_fields(self, streams):
+        """
+        This will return the non_selected_fields for each streams
+        """     
+        non_selected_fields = {}
+        
+        for stream in streams:
+            # For below streams all fields needs to be selected
+            if stream in ["Audience Overview", "Audience Geo Location", "Acquisition Overview"]:
+                non_selected_fields[stream] = set()
+
+            # For Behavior Overview stream, except "ga:searchKeyword", "ga:eventCategory" all fields needs to be selected
+            elif stream == "Behavior Overview":
+                non_selected_fields[stream] = {"ga:searchKeyword", "ga:eventCategory"}
+            
+            # This will randomly select the dimension and metric for the stream
+            else:
+                print(f'Randomly selecting metrics and dimensions for stream: {stream}')
+                dimension = self.stream_dimension.get(stream) # Getting the dimension
+                metric = self.stream_metric.get(stream) # Getting the metrics
+                # Randomly select the dimension
+                selected_dimension_fields = self.random_field_selection(dimension, NO_OF_DIMENSIION_ALLOWED) 
+                print(f'Random Combination of dimension for {stream} : {selected_dimension_fields}')
+                # Randomly select the metrics
+                selected_metrics_fields = self.random_field_selection(metric, NO_OF_METRIC_ALLOWED)
+                print(f'Random Combination of metrics for {stream} : {selected_metrics_fields} ')
+                non_selected_fields[stream] = dimension | metric - selected_dimension_fields - selected_metrics_fields 
         return non_selected_fields
 
+    def validate_selected_fields(self, selected_fields):
+        """
+        This function will validate the combination of randomly selected dimension and metrics
+        """
+        for field, conflict_fields in self.conflicting_dimension.items():
+            # If any field in selected_fields, then discard conflict_field from the selected_fields
+            if field in selected_fields:
+                for conflict_field in conflict_fields:
+                    selected_fields.discard(conflict_field)
+
+    def random_field_selection(self, fields, no_of_fields):
+        """
+        This function will randomly select the dimensions and metrics
+        """
+        if len(fields) > no_of_fields:
+            fields = set(random.sample(fields, no_of_fields))
+        self.validate_selected_fields(fields)
+        return fields
+
     def test_run(self):
+
+        # As Audience Technology stream has random field selection, running that stream 5 times
+        for i in range(5):
+            expected_streams = {"Audience Technology"}
+            self.run_temp(expected_streams)
+        # For other streams, running the test case once
+        expected_streams = self.expected_sync_streams() - self.SKIP_STREAMS
+        self.run_temp(expected_streams)
+
+    def run_temp(self, expected_streams):
         """
         • Verify no unexpected streams were replicated.
         • Verify that more than just the automatic fields are replicated for each stream. 
         • verify the selected fields for each Audience Technology streams are replicated.
         """
 
-        expected_streams = {"Audience Technology"}
-
-        non_selected_properties_by_stream = {
-            "Audience Technology":self.get_non_selected_fields()
-            }
+        # Grabbing the non_selected_props for the streams
+        non_selected_properties_by_stream = self.get_non_selected_fields(expected_streams)
 
         expected_automatic_fields = self.expected_automatic_fields()
         conn_id = connections.ensure_connection(self)
@@ -83,5 +154,5 @@ class GoogleAnalyticsDynamicFieldselectionTest(GoogleAnalyticsBaseTest):
                     if message['action'] == 'upsert':
                         actual_all_keys.update(message['data'].keys())
 
-                # verify all the selected fields for each Audience Technology are replicated
+                # verify all the selected fields for each streams are replicated
                 self.assertSetEqual(expected_all_keys, actual_all_keys)
