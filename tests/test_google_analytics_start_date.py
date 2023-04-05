b0f4c5e2d16512a1f94ebee4e78c4dcde3c1e793
bhuvana-talend <126521351+bhuvana-talend@users.noreply.github.com>
2023-04-05 08:27:17 -0700

    Excluded the last 2 days in assertion for primary keys list (#80)
    * Excluded the last 2 days in assertion for primary keys list
    
    * call pendulum.today() instead of now to compare the dates against midnight and moved the golden date to a variable
    
    * Removed unnecessary white spaces

--- tests/test_google_analytics_start_date.py
+++ tests/test_google_analytics_start_date.py
@@ -81,17 +81,20 @@ class GoogleAnalyticsStartDateTest(GoogleAnalyticsBaseTest):
                 expected_start_date_2 = self.start_date_2
 
                 # collect information for assertions from syncs 1 & 2 base on expected values
+                # Exclude the last 2 days to avaoid instability in the tests- TDL-22494
                 record_count_sync_1 = record_count_by_stream_1.get(stream, 0)
                 record_count_sync_2 = record_count_by_stream_2.get(stream, 0)
+
+                sync_end_date = pendulum.today().subtract(days=2);
+
                 primary_keys_list_1 = [tuple(message.get('data').get(expected_pk) for expected_pk in expected_primary_keys)
                                        for message in synced_records_1.get(stream).get('messages')
-                                       if message.get('action') == 'upsert' and pendulum.parse(message.get('data').get('start_date')) < pendulum.now().subtract(days=2) ]
+                                       if message.get('action') == 'upsert' and pendulum.parse(message.get('data').get('start_date')) < sync_end_date]
                 primary_keys_list_2 = [tuple(message.get('data').get(expected_pk) for expected_pk in expected_primary_keys)
                                        for message in synced_records_2.get(stream).get('messages')
-                                       if message.get('action') == 'upsert' and pendulum.parse(message.get('data').get('start_date')) < pendulum.now().subtract(days=2) ]
+                                       if message.get('action') == 'upsert' and pendulum.parse(message.get('data').get('start_date')) < sync_end_date ]
                 primary_keys_sync_1 = set(primary_keys_list_1)
                 primary_keys_sync_2 = set(primary_keys_list_2)
-
 
                 # collect information specific to incremental streams from syncs 1 & 2
                 expected_replication_key = next(iter(self.expected_replication_keys().get(stream)))
